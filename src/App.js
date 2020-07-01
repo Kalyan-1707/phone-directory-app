7ae2c07fa218425e6f406ec0a60eece5a4904f31
Kalyan-2707 <52063829+Kalyan-2707@users.noreply.github.com>
2020-07-01 17:43:23 +0530

    Created Skeleton for Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div >
+    <div>
+      <div className="header">
+        Phone Directory
+      </div>
+      <button>
+        Add
+      </button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span><br/>
+      </div>
       
     </div>
   );
