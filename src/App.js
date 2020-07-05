a6ed82475b6dc82ce77186c4dd5298cf88de6c6d
Kalyan-2707 <52063829+Kalyan-2707@users.noreply.github.com>
2020-07-05 22:41:51 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,24 +3,72 @@ import './App.css'
 import Header from './Header'
 
 function App() {
+
+  let subs=[
+    {
+      id : 1,
+      name : 'Alpha',
+      contact : '987654321'
+    },
+    {
+      id : 2,
+      name : 'Hydra',
+      contact : '123456789'
+
+    }
+  ]
   return (
     <div>
       <Header />
       <button className="addbtn waves-effect">
         Add
       </button>
-      <div className="row">
-        
 
-    <div className='col m4 s6'>
-        <span className="heading">Name</span> 
+      
+      <div className="row">
+
+
+        <div className='col m4 s6'>
+          <span className="heading">Name</span>
         </div>
 
         <div className='col m4 s6'>
-        <span className='heading'>Phone</span>
+          <span className='heading'>Phone</span>
         </div>
-      </div>
       
+      </div>
+
+      {
+
+        subs.map( sub => {
+
+          return (
+
+            <div className="row" key={sub.id}>
+
+              <div className='col m4 s6'>
+          <span className="sub-data">{sub.name}</span>
+              </div>
+
+              <div className='col m4 s6'>
+          <span className='sub-data'>{sub.contact}</span>
+              </div>
+
+
+            </div>
+
+
+          )
+
+        } )
+  
+      
+      }
+
+
+
+
+
     </div>
   );
 }
